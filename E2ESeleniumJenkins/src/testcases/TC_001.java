45c2bf57342feaca9fdb62a49dbd05a65a9e7635
sahithi <sahithi@WINDOWS-5Q8NUMT.T-mobile.com>
2020-12-29 22:54:02 -0800

    New E2E Integration code into GitHub

---
+++ E2ESeleniumJenkins/src/testcases/TC_001.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+	public void StartDriver() {
+		
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("Hello Sahithi");
+		driver.findElement(By.id("pass")).sendKeys("1234");
+		driver.quit();
+		
+	}
+
+}
---
+++ E2ESeleniumJenkins/src/testcases/TC_002.java
@@ -0,0 +1,18 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC_002 {
+	
+	public void StartDriver() {
+		
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("Hello Sahithi");
+		driver.findElement(By.id("pass")).sendKeys("1234");
+		
+	}
+
+}
